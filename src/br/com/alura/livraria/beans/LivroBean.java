73d93f85ac5474a73e946e29aa996aef3a287a8f
Isaac <isaac.benites@hotmail.com>
2020-06-24 10:27:11 -0300

    Primeira aula, iniciando a livraria

---
+++ src/br/com/alura/livraria/beans/LivroBean.java
@@ -0,0 +1,18 @@
+package br.com.alura.livraria.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo() +" na base de dados");
+	}
+
+}
